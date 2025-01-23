3dcf9c24d14f42f22f3cc98ad51772b41d81605c
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-01-23 11:54:20 -0300

    atualização

--- repairshop/drizzle.config.ts
+++ repairshop/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-    schema: "./app/db/schema.ts",
-    out: "./app/db/migrations",
+    schema: "./src/db/schema.ts",
+    out: "./src/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
