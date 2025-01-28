6e0358547cb1fd293cca36563484a7adada6963b
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-01-28 15:54:14 -0300

    atualização

--- repairshop/src/app/(rs)/customers/page.tsx
+++ repairshop/src/app/(rs)/customers/page.tsx
@@ -1,7 +1,27 @@
+import CustomerSearch from "@/app/(rs)/customers/CustomerSearch"
+import { getCustomerSearchResult } from "@/lib/queries/getCustomerSearchResults"
+
 export const metadata = {
-    title: "Customers",
+    title: "Customers Search",
 }
 
-export default function Customers() {
-    return <h2>Customers Page</h2>
+export default async function Customers({
+    searchParams,
+}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    const { searchText } = await searchParams
+
+    if (!searchText) return <CustomerSearch />
+
+    // query database
+    const results = await getCustomerSearchResult(searchText)
+
+    // return results
+    return (
+        <>
+            <CustomerSearch />
+            <p>{JSON.stringify(results)}</p>
+        </>
+    )
 }
